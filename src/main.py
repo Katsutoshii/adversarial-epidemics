3ceb93e0932e78abc617c7999ca04a865a2b7e14
Josiah Putman <joshikatsu@gmail.com>
2020-02-10 17:20:29 -0500

    Setup

---
+++ src/main.py
@@ -0,0 +1,10 @@
+'''
+File: main.py
+Project: src
+File Created: Monday, 10th February 2020 4:26:06 pm
+Author: Josiah Putman (joshikatsu@gmail.com)
+-----
+Last Modified: Monday, 10th February 2020 4:26:11 pm
+Modified By: Josiah Putman (joshikatsu@gmail.com)
+'''
+

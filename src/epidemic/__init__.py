0e7c1d9bb30be67c45e9c1e1a1247683eff3023b
Josiah <joshikatsu@gmail.com>
2020-02-12 03:25:47 -0500

    Added paper

---
+++ src/epidemic/__init__.py
@@ -0,0 +1,67 @@
+'''
+File: __init__.py
+Project: epidemic
+File Created: Monday, 10th February 2020 4:25:50 pm
+Author: Josiah Putman (joshikatsu@gmail.com)
+-----
+Last Modified: Monday, 10th February 2020 5:18:30 pm
+Modified By: Josiah Putman (joshikatsu@gmail.com)
+'''
+
+from typing import List, Set
+from dataclasses import dataclass
+import networkx as nx
+import matplotlib.pyplot as plt
+
+@dataclass
+class Epidemic():
+    spread_fraction: float = 0.5
+    incubation_period: int = 0
+    
+@dataclass
+class EpidemicGraph():
+    G: nx.Graph
+    ep: Epidemic
+    susceptible: Set[int] = set()
+    infected: Set[int] = set()
+
+    def __post_init__(self):
+        """
+        Run after the init function finishes
+        """
+        
+    def step(self):
+        """
+        Runs a timestep of the simulated epidemic on the graph
+        """
+        pass
+
+    def draw(self):
+        pos = nx.spring_layout(G) # positions for all nodes
+        nx.draw_networkx_nodes(G, pos,
+                       nodelist=[0,1,2,3],
+                       node_color='r',
+                       node_size=500,
+                   alpha=0.8)
+        nx.draw_networkx_nodes(G, pos,
+                            nodelist=[4,5,6,7],
+                            node_color='b',
+                            node_size=500,
+                        alpha=0.8)
+
+        # edges
+        nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
+        nx.draw_networkx_edges(G,pos,
+                            edgelist=[(0,1),(1,2),(2,3),(3,0)],
+                            width=8,alpha=0.5,edge_color='r')
+        nx.draw_networkx_edges(G,pos,
+                            edgelist=[(4,5),(5,6),(6,7),(7,4)],
+                            width=8,alpha=0.5,edge_color='b')
+    
+if __name__ == "__main__":
+    G: nx.Graph = nx.fast_gnp_random_graph(n=20, p=20)
+    ep: Epidemic = Epidemic()
+    EG: EpidemicGraph = EpidemicGraph(G, ep, infected=set([1]))
+    EG.draw()
+    plt.show()
+    

1e741c54129fe40e696a0d67c1de75dcea0a2087
Aarish Iyer <aarish407@gmail.com>
2020-03-04 17:33:54 -0500

    Changed calculate_errors to print generated vs truth values for China

--- src/tools/calculate_errors.py
+++ src/tools/calculate_errors.py
@@ -19,7 +19,7 @@ def calculate_diff(generated, truth, k):
             continue
 
         for index in range(data_points):
-            diff[country]['cases']+= (generated[country]['cases'][index] - truth[country]['cases'][index])**2
+            diff[country]['cases']+= (generated[country]['cases'][index]  - truth[country]['cases'][index])**2
             diff[country]['deaths']+= (generated[country]['deaths'][index] - truth[country]['deaths'][index])**2
             diff[country]['recoveries']+= (generated[country]['recoveries'][index] - truth[country]['recoveries'][index])**2
         
@@ -51,13 +51,36 @@ def plot_max_error_countries(generated, truth, countries):
             diff[country]['deaths'].append((generated[country]['deaths'][index] - truth[country]['deaths'][index])**2)
             diff[country]['recoveries'].append((generated[country]['recoveries'][index] - truth[country]['recoveries'][index])**2)   
     
-    for country in diff:        
-        fig, ax = plt.subplots()
-        ax.set_title(country)
-        ax.plot(diff[country]['cases'], marker='o', markersize=12,  linewidth= 4, label= 'cases')
-        ax.plot(diff[country]['recoveries'], marker='o', markersize=12, linewidth= 4, label= 'recoveries')
-        ax.plot(diff[country]['deaths'], marker='o', markersize=12,  linewidth= 4, label= 'deaths')
-        ax.legend()
+    # for country in diff:        
+    #     fig, ax = plt.subplots()
+    #     ax.set_title(country)
+    #     ax.plot(diff[country]['cases'], marker='o', markersize=12,  linewidth= 4, label= 'cases')
+    #     ax.plot(diff[country]['recoveries'], marker='o', markersize=12, linewidth= 4, label= 'recoveries')
+    #     ax.plot(diff[country]['deaths'], marker='o', markersize=12,  linewidth= 4, label= 'deaths')
+    #     ax.legend()
+
+    # cumulative_cases= [x + y + z for x, y, z in zip(generated["China"]['cases'], generated["China"]['deaths'], generated["China"]['recoveries'])]
+    cumulative_cases= []
+    for index in range(len(generated["China"]['cases'])):
+        cumulative_cases.append(generated["China"]['cases'][index] + generated["China"]['deaths'][index] + generated["China"]['recoveries'][index])
+
+    fig1, ax1= plt.subplots()
+    ax1.set_title("China cases")
+    ax1.plot(cumulative_cases, marker='o', markersize=12,  linewidth= 4, label= 'generated')
+    ax1.plot(truth["China"]['cases'], marker='o', markersize=12, linewidth= 4, label= 'truth')
+    ax1.legend()
+
+    fig2, ax2= plt.subplots()
+    ax2.set_title("China deaths")
+    ax2.plot(generated["China"]['deaths'], marker='o', markersize=12,  linewidth= 4, label= 'generated')
+    ax2.plot(truth["China"]['deaths'], marker='o', markersize=12, linewidth= 4, label= 'truth')
+    ax2.legend()
+
+    fig2, ax2= plt.subplots()
+    ax2.set_title("China recoveries")
+    ax2.plot(generated["China"]['recoveries'], marker='o', markersize=12,  linewidth= 4, label= 'generated')
+    ax2.plot(truth["China"]['recoveries'], marker='o', markersize=12, linewidth= 4, label= 'truth')
+    ax2.legend()    
     
     plt.show()
     
